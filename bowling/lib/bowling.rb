5337d440f8ea6e73551b6afdca4a2cdd1f218699
Sandi Metz <sandi@sandimetz.com>
2018-08-22 05:26:51 -0400

    Parse/exec trial code to score complete games with spares/open frames

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -6,6 +6,23 @@ class Bowling
   end
 
   def score
+    sum_of_frames
     rolls.sum
+  end
+
+  def sum_of_frames(running_score = 0, remaining_rolls = rolls.dup)
+    if remaining_rolls.size > 2 and (remaining_rolls.take(2).sum) == 10
+      running_score += remaining_rolls[0] + remaining_rolls[1] + remaining_rolls[2]
+      return sum_of_frames(running_score, remaining_rolls.drop(2))
+    end
+
+    if remaining_rolls.size > 1
+      running_score += remaining_rolls[0] + remaining_rolls[1]
+      if remaining_rolls.drop(2).empty?
+        running_score
+      else
+        return sum_of_frames(running_score, remaining_rolls.drop(2))
+      end
+    end
   end
 end
