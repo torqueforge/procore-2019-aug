fc0ea97bfd5b09066d0ca5362ef96dbf64be2919
Sandi Metz <sandi@sandimetz.com>
2018-08-22 05:29:52 -0400

    Add code to score opening strike in complete game, plus comments

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -10,11 +10,19 @@ class Bowling
   end
 
   def sum_of_frames(running_score = 0, remaining_rolls = rolls.dup)
+    # strike
+    if remaining_rolls.size > 2 and (remaining_rolls.take(1).sum) == 10
+      running_score += remaining_rolls[0] + remaining_rolls[1] + remaining_rolls[2]
+      return sum_of_frames(running_score, remaining_rolls.drop(1))
+    end
+
+    # spare
     if remaining_rolls.size > 2 and (remaining_rolls.take(2).sum) == 10
       running_score += remaining_rolls[0] + remaining_rolls[1] + remaining_rolls[2]
       return sum_of_frames(running_score, remaining_rolls.drop(2))
     end
 
+    # open frame
     if remaining_rolls.size > 1
       running_score += remaining_rolls[0] + remaining_rolls[1]
       if remaining_rolls.drop(2).empty?
