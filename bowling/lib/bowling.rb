69f850e7e017fc0b8d76e396de6ad9935ed6f70c
Sandi Metz <sandi@sandimetz.com>
2018-08-22 05:46:45 -0400

    Keep track of current_frame so I'll know when to stop scoring.

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -9,17 +9,24 @@ class Bowling
     sum_of_frames
   end
 
-  def sum_of_frames(running_score = 0, remaining_rolls = rolls.dup)
+  def sum_of_frames(running_score = 0, remaining_rolls = rolls.dup,
+                    current_frame = 0, max_frames = 10)
+
+    current_frame += 1
+    if current_frame > max_frames
+      return running_score
+    end
+
     # strike
     if remaining_rolls.size > 2 and (remaining_rolls.take(1).sum) == 10
       running_score += remaining_rolls[0] + remaining_rolls[1] + remaining_rolls[2]
-      return sum_of_frames(running_score, remaining_rolls.drop(1))
+      return sum_of_frames(running_score, remaining_rolls.drop(1), current_frame)
     end
 
     # spare
     if remaining_rolls.size > 2 and (remaining_rolls.take(2).sum) == 10
       running_score += remaining_rolls[0] + remaining_rolls[1] + remaining_rolls[2]
-      return sum_of_frames(running_score, remaining_rolls.drop(2))
+      return sum_of_frames(running_score, remaining_rolls.drop(2), current_frame)
     end
 
     # open frame
@@ -28,7 +35,7 @@ class Bowling
       if remaining_rolls.drop(2).empty?
         running_score
       else
-        return sum_of_frames(running_score, remaining_rolls.drop(2))
+        return sum_of_frames(running_score, remaining_rolls.drop(2), current_frame)
       end
     end
   end
