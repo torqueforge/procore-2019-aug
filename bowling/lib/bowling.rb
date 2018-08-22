20647a1862afd314d78aba409c21608e7b58b928
Sandi Metz <sandi@sandimetz.com>
2018-08-22 06:24:01 -0400

    Comment all the reasons to stop scoring

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -13,10 +13,10 @@ class Bowling
                     current_frame = 0, max_frames = 10)
 
     current_frame += 1
-    if current_frame > max_frames ||
+    if current_frame > max_frames ||          # stop if game over
       (((remaining_rolls.take(1).sum) == 10) or ((remaining_rolls.take(2).sum) == 10) and
-          remaining_rolls.size < 3) ||
-      (remaining_rolls.size < 2)
+          remaining_rolls.size < 3) ||        # stop if strike or spare needs more rolls
+      (remaining_rolls.size < 2)              # stop if frame needs more rolls
       return running_score
     end
 
